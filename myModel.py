b769c7f7960144900f6f6116cf89ffe7b7bb98a1
benlumsden2 <70603404+benlumsden2@users.noreply.github.com>
2023-02-23 15:28:33 +0000

    add trees

--- myModel.py
+++ myModel.py
@@ -8,8 +8,7 @@ EMPTY, TREE, FIRE, BREAK, SETTLE = 0, 1, 2, 3, 4
 np.random.seed(2)
 system = np.random.random([width, height])
 
-system[system <= 0.6] = 1
-system[(system > 0.6) and (system != 1)] = 0
+system = np.where(system <= 0.6, 1, 0)
 
 print(system)
 
