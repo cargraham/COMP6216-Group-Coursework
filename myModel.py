06b6a20879dd1f48d65d3a6b430c3eba3e8b9838
benlumsden2 <70603404+benlumsden2@users.noreply.github.com>
2023-02-24 23:15:25 +0000

    fix seeding

--- myModel.py
+++ myModel.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 import random
 
+SEED_VAL = 1
 WIDTH, HEIGHT = 100, 100
 TREE_PROB = 0.6
 NUM_SETTLEMENTS = 5
@@ -14,18 +15,18 @@ EMPTY, TREE, SETTLE, FIRE, BREAK = 0, 1, 2, 3, 4
 colours = ['white', 'green', 'brown']
 cmap = colors.ListedColormap(colours)
 
-np.random.seed(2)
+np.random.seed(SEED_VAL)
 system = np.random.random([WIDTH, HEIGHT])
 system = np.where(system <= TREE_PROB, TREE, EMPTY)
 
 def make_settlements(system, num, min_size, max_size):
     for i in range(num):
-        random.seed(i+1)
+        random.seed(SEED_VAL + i+1)
         size = random.randint(min_size, max_size)
         center = (random.randint(size, system.shape[0]-size-1), random.randint(size, system.shape[1]-size-1))
 
         shape_size = size if size % 2 == 1 else size + 1
-        np.random.seed(i+1)
+        np.random.seed(SEED_VAL + i+1)
         shape = np.random.choice([0, 1], size=(shape_size, shape_size), p=[0.4, 0.6])
 
         x1, y1 = center[0] - shape_size//2, center[1] - shape_size//2
