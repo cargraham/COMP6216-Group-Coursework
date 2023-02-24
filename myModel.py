d8a78ffebadd33c42cddbeb0928021fcb514a6aa
Charlotte <cb6g19@soton.ac.uk>
2023-02-24 19:06:50 +0000

    initial prototype for grid with trees and settlements

--- myModel.py
+++ myModel.py
@@ -1,16 +1,47 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import math
 
 width, height = 100, 100
 tree_prob = 0.6
+settle_prob = 0.5
+neighbours_prob = 0.1
+settle_num = 5
+neighbours = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
 
-EMPTY, TREE, FIRE, BREAK, SETTLE = 0, 1, 2, 3, 4
+EMPTY, TREE, SETTLE, FIRE, BREAK,  = 0, 1, 2, 3, 4
+
+colours = ['white', 'green', 'brown']
+            # 'orange', 'black']
+cmap = colors.ListedColormap(colours)
 
 np.random.seed(2)
 system = np.random.random([width, height])
 
-system = np.where(system <= 0.6, 1, 0)
+system = np.where(system <= tree_prob, TREE, EMPTY)
 
-print(system)
+def settle_neighbours(x, y):
+    system[x][y] = SETTLE
+    for n in neighbours:
+        neigh_x = x + n[0]
+        neigh_y = y + n[1]
+        if((neigh_x >= 0 and neigh_x < width) and (neigh_y >= 0 and neigh_y < height)):
+            if(np.random.random() <= settle_prob):
+                system[neigh_x][neigh_y] = SETTLE
+            if(np.random.random() <= neighbours_prob):
+                settle_neighbours(neigh_x, neigh_y)
+
+# generate settlements
+for settle in range(settle_num):
+    x = math.trunc(np.random.random() * 100)
+    y = math.trunc(np.random.random() * 100)
+    settle_neighbours(x, y)
+
+plt.imshow(system, cmap=cmap)
+plt.show()
+
+# print(system)
 
 np.savetxt('system.txt', system, fmt='%.1i')
         
