90eae81a9e0fc6b824f174409c9574b8f839d7ad
benlumsden2 <70603404+benlumsden2@users.noreply.github.com>
2023-02-24 23:13:42 +0000

    random settlements

--- myModel.py
+++ myModel.py
@@ -1,48 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
-import math
+import random
 
-width, height = 100, 100
-tree_prob = 0.6
-settle_prob = 0.6
-settle_num = 5
-neighbours = [(-2, -2), (-1, -2), (0, -2), (1, -2), (2, -2),
-              (-2, -1), (-1, -1), (0, -1), (1, -1), (2, -1),
-              (-2, 0), (-1, 0), (1, 0), (2, 0),
-              (-2, 1), (-1, 1), (0, 1), (1, 1), (2, 1),
-              (-2, 2), (-1, 2), (0, 2), (1, 2), (2, 2)]
+WIDTH, HEIGHT = 100, 100
+TREE_PROB = 0.6
+NUM_SETTLEMENTS = 5
+MIN_SETTLE_SIZE = 5
+MAX_SETTLE_SIZE = 10
 
-EMPTY, TREE, SETTLE, FIRE, BREAK,  = 0, 1, 2, 3, 4
+EMPTY, TREE, SETTLE, FIRE, BREAK = 0, 1, 2, 3, 4
 
 colours = ['white', 'green', 'brown']
-            # 'orange', 'black']
 cmap = colors.ListedColormap(colours)
 
 np.random.seed(2)
-system = np.random.random([width, height])
+system = np.random.random([WIDTH, HEIGHT])
+system = np.where(system <= TREE_PROB, TREE, EMPTY)
 
-system = np.where(system <= tree_prob, TREE, EMPTY)
+def make_settlements(system, num, min_size, max_size):
+    for i in range(num):
+        random.seed(i+1)
+        size = random.randint(min_size, max_size)
+        center = (random.randint(size, system.shape[0]-size-1), random.randint(size, system.shape[1]-size-1))
 
-def settle_neighbours(x, y):
-    system[x][y] = SETTLE
-    for n in neighbours:
-        neigh_x = x + n[0]
-        neigh_y = y + n[1]
-        if((neigh_x >= 0 and neigh_x < width) and (neigh_y >= 0 and neigh_y < height)):
-            if(np.random.random() <= settle_prob):
-                system[neigh_x][neigh_y] = SETTLE
+        shape_size = size if size % 2 == 1 else size + 1
+        np.random.seed(i+1)
+        shape = np.random.choice([0, 1], size=(shape_size, shape_size), p=[0.4, 0.6])
 
-# generate settlements
-for settle in range(settle_num):
-    x = math.trunc(np.random.random() * 100)
-    y = math.trunc(np.random.random() * 100)
-    settle_neighbours(x, y)
+        x1, y1 = center[0] - shape_size//2, center[1] - shape_size//2
+        x2, y2 = center[0] + shape_size//2, center[1] + shape_size//2
+        system[x1:x2+1, y1:y2+1][shape==1] = SETTLE
+
+make_settlements(system, NUM_SETTLEMENTS, MIN_SETTLE_SIZE, MAX_SETTLE_SIZE)
 
 plt.imshow(system, cmap=cmap)
 plt.show()
 
-# print(system)
-
 np.savetxt('system.txt', system, fmt='%.1i')
-        
